e276c036341c0aab3ea9e4030159b50ad82e9937
Archie-L <526737@chrome.escg.ac.uk>
2022-04-25 12:00:49 +0100

    updated the boss

--- Assets/GodjamesBoss.cs
+++ Assets/GodjamesBoss.cs
@@ -5,11 +5,36 @@ using UnityEngine.AI;
 
 public class GodjamesBoss : MonoBehaviour
 {
-
+	public NavMeshAgent agent;
+	public Transform player;
+	public Animator anim;
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
-    }
+		agent.speed = 3f;
+	}
+
+	private void Update()
+	{
+		agent.SetDestination(player.position);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			anim.SetBool("Walk", false);
+			agent.speed = 0f;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			anim.SetBool("Walk", true);
+			agent.speed = 3f;
+		}
+	}
 }
